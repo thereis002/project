10d76cb0baeb8cbfc9a652d484b2dca602037b28
Lucas Dias <lucas_rmd@icloud.com>
2023-12-19 17:55:14 +0100

    chore: project bootstrap

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import logo from "./logo.svg";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Buyflow, { ProductIds } from "./buyflow/Buyflow";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <Switch>
+          <Route path="/buy/insurance_dev">
+            <Buyflow productId={ProductIds.devIns} />
+          </Route>
+          <Route path="/">
+            <p>Welcome to Getsafe's Developer Insurance</p>
+            <Link to="/buy/insurance_dev">Get started!</Link>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
